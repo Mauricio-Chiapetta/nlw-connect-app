15dce617c23094fa756565dde6f8e52c4efba223
Mauricio Chiapetta <m.faisca1234@gmail.com>
2025-02-27 18:52:14 -0300

    finalizando UI segunda tela (invite)

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,34 +1,29 @@
-import type { ComponentProps } from "react";
+import type { ComponentProps } from 'react'
 
-interface inputIconProps extends ComponentProps<"div"> {
-  error?: boolean;
+interface inputRootProps extends ComponentProps<'div'> {
+  error?: boolean
 }
-export function InputRoot({ error = false, ...props }: inputIconProps) {
+export function InputRoot({ error = false, ...props }: inputRootProps) {
   return (
     <div
       data-error={error}
       className="group bg-gray-800 h-12 border border-gray-600 px-4 rounded-xl flex items-center gap-2 focus-within:border-gray-100 data-[error=true]:border-danger"
       {...props}
     />
-  );
+  )
 }
 
-interface inputRootProps extends ComponentProps<"span"> {}
-export function InputIcon(props: inputRootProps) {
+interface inputIconProps extends ComponentProps<'span'> {}
+export function InputIcon(props: inputIconProps) {
   return (
     <span
       className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
       {...props}
     />
-  );
+  )
 }
 
-interface inputFieldProps extends ComponentProps<"input"> {}
+interface inputFieldProps extends ComponentProps<'input'> {}
 export function InputField(props: inputFieldProps) {
-  return (
-    <input
-      className="flex-1 outline-0 placeholder-gray-400 text-gray-100"
-      {...props}
-    />
-  );
+  return <input className="flex-1 outline-0 placeholder-gray-400" {...props} />
 }
